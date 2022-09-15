a7a4a5e9c704e39444c787a6b2e538d87d639817
Peter Janser <pj@janser-it.ch>
2022-09-15 18:33:08 +0200

    Update peerDependency constraints; Pump those numbers up!

--- resolverFactory.js
+++ resolverFactory.js
@@ -1,12 +1,12 @@
-import { pathToFileURL } from 'url'
+import { pathToFileURL } from 'url';
 
-import { matchPath } from './matchPath.js'
+import { matchPath } from './matchPath.js';
 
-export const resolverFactory = (resolveTs) => {
-    return function (specifier, ctx, defaultResolve) {
-        const match = matchPath && matchPath(specifier)
+export const resolverFactory = ( resolveTs ) => {
+    return function ( specifier, ctx, defaultResolve ) {
+        const match = matchPath && matchPath( specifier );
         return match
-          ? resolveTs(pathToFileURL(`${match}`).href, ctx, defaultResolve)
-          : resolveTs(specifier, ctx, defaultResolve)
-      }
-}
+            ? resolveTs( pathToFileURL( `${match}` ).href, ctx, defaultResolve )
+            : resolveTs( specifier, ctx, defaultResolve );
+    };
+};
